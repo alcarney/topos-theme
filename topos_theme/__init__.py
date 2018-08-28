236385f2fbb6c39d87c2d7737e3da60e56113e5d
Alex Carney <alcarneyme@gmail.com>
2018-08-28 19:05:16 +0100

    Update to v0.0.5
    - Improved layout (in my opinion :) )
      + Sidebar now on the left
      + White background throughout, fewer coloured borders
    
    - Better font choice, inline code is more obvious now
    
    - Improved admonition styles, they take up less space and (hopefully) more
      interesting to look at
    
    - Navigation sections now give some feedback showing which section the user is
      currently in

--- topos_theme/__init__.py
+++ topos_theme/__init__.py
@@ -1,8 +1,18 @@
 from os import path
 
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
+
+
+def update_context(app, pagename, templatename, context, doctree):
+    context["topos_theme_version"] = __version__
 
 
 def setup(app):
-    app.add_html_theme('topos-theme', path.abspath(path.dirname(__file__)))
+
+    if hasattr(app, "add_html_theme"):
+        app.add_html_theme('topos-theme', path.abspath(path.dirname(__file__)))
+
+    app.connect('html-page-context', update_context)
+
+    return {"version": __version__, "parallel_read_safe": True}
